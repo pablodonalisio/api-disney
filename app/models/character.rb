0746f4a1f6ed6805e261ace290e1e41f50ff3969
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-05-19 16:40:01 -0300

    Validate presence on Character name, and set default to age, weight and story

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,10 @@
 class Character < ApplicationRecord
+  validates :name, presence: true
+  after_initialize :init
+
+  def init
+    self.age ||= 'unknown'
+    self.weight ||= 'unknown'
+    self.story ||= 'unknown'
+  end
 end
