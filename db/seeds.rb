858db6c29ee8a003e00000923488ac8bbcf97c06
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-05-19 16:26:37 -0300

    Create Character scaffold and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Character.create(
+  {
+    name: 'Alladin',
+    weight: 72,
+    story: 'When Aladdin is initially introduced, he is eighteen years old.
+            He never received a formal education, and has only learned by living on the streets
+            of Agrabah. He has to steal food in the local market in order to survive.
+            He was born to Cassim and his wife. When Aladdin was only an infant, his father
+            left him and his mother in order to find a better life for his family.',
+    age: 18
+  }
+)
+
+Character.create(
+  {
+    name: 'Mickey Mouse',
+    weight: 'unknown',
+    story: "Mickey Mouse is a cartoon character created in 1928 by The Walt Disney Company,
+            who also serves as the brand's mascot. An anthropomorphic mouse who typically
+            wears red shorts, large yellow shoes, and white gloves, Mickey is one of
+            the world's most recognizable fictional characters.",
+    age: 'unknown'
+  }
+)
