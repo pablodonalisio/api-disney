e7d724c49a8e45c942b58bd220be282eee5276fc
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-05-21 15:50:36 -0300

    Remove default values on Character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,12 +1,4 @@
 class Character < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_and_belongs_to_many :films
-  after_initialize :init
-
-  def init
-    self.image_url ||= 'unknown'
-    self.age ||= 'unknown'
-    self.weight ||= 'unknown'
-    self.story ||= 'unknown'
-  end
 end
