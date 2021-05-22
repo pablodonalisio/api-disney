c69ea27d65722d1cb32b220e4fabe6423d0112b3
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-05-22 14:13:01 -0300

    Create user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_202537) do
+ActiveRecord::Schema.define(version: 2021_05_22_170839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2021_05_20_202537) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
