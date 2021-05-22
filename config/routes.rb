c5bfb2c5d5f9acf85adb41a4b98a3075c92db249
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-05-22 14:12:40 -0300

    Add routes for login and users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :genres, only: %i[create update destroy]
   resources :films, path: 'movies'
   resources :characters
+  resources :users
+  get 'login', to: 'authentication#login'
 end
